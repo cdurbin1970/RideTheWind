f5f8e04327858f20596fe2fda8b07dbae9c6226c
Carey Durbin <cdurbin1970@gmail.com>
2023-12-19 21:19:14 -0500

    - Added code to control the backward force of the rudder - Added code to increase the steering speed - Added additional sections and options to the config file

--- RideTheWind.cs
+++ RideTheWind.cs
@@ -14,12 +14,15 @@ namespace RideTheWind {
         private static ConfigEntry<bool> _windEnabled;
         private static ConfigEntry<bool> _sailEnabled;
         private static ConfigEntry<bool> _useWindIntensity;
+        private static ConfigEntry<bool> _rudderEnabled;
         private static ConfigEntry<bool> _useAngle;
         private static ConfigEntry<float> _angle;
         private static ConfigEntry<bool> _safety; 
         private static ConfigEntry<float> _speedFullSail;
         private static ConfigEntry<float> _speedHalfSail;
         private static ConfigEntry<float> _windIntensity;
+        private static ConfigEntry<float> _rudderBackwardForce;
+        private static ConfigEntry<float> _rudderSpeed;
 
         private void Awake() {
         /*
@@ -31,13 +34,16 @@ namespace RideTheWind {
             BepInExPlugin._safety = this.Config.Bind("General", "SafeSettings", true, "Use safe settings? When safe is true, will only work with Longship and 6 is max speed.");
             BepInExPlugin._windEnabled = this.Config.Bind("General", "WindEnabled", true, "Enable the wind behind you?");
             BepInExPlugin._sailEnabled = this.Config.Bind("General", "SailEnabled", true, "Enable the wind speed multiplier?");
+            BepInExPlugin._rudderEnabled = this.Config.Bind("General", "RudderEnabled", true, "Enable the rudder changes?");
             BepInExPlugin._useWindIntensity = this.Config.Bind("General", "WindIntensityEnabled", false, "Enable the wind intensity?");
-            BepInExPlugin._useAngle = this.Config.Bind("General", "UseThisAngle", false, "Want to set a specific angle?");
-            BepInExPlugin._angle = this.Config.Bind("General", "TheAngle", 0.0f, "Wind angle relative to ship");
-            BepInExPlugin._windIntensity = this.Config.Bind("General", "WindIntensity", 0.05f, "Set the wind intensity (0.00-1 MAX");
-            BepInExPlugin._speedHalfSail = this.Config.Bind("General", "HalfSailWindSpeed", 1.0f, "Wind speed multiplier for half sail (1-6 MAX)");
-            BepInExPlugin._speedFullSail = this.Config.Bind("General", "FullSailWindSpeed", 1.0f, "Wind speed multiplier for full sail (1-6 MAX)");
-            
+            BepInExPlugin._useAngle = this.Config.Bind("Wind", "UseThisAngle", false, "Want to set a specific angle?");
+            BepInExPlugin._angle = this.Config.Bind("Wind", "TheAngle", 0.0f, "Wind angle relative to ship");
+            BepInExPlugin._windIntensity = this.Config.Bind("Wind", "WindIntensity", 0.05f, "Set the wind intensity (0.00-1 MAX");
+            BepInExPlugin._speedHalfSail = this.Config.Bind("Sail", "HalfSailWindSpeed", 2.0f, "Wind speed multiplier for half sail (1-6 MAX)");
+            BepInExPlugin._speedFullSail = this.Config.Bind("Sail", "FullSailWindSpeed", 4.0f, "Wind speed multiplier for full sail (1-6 MAX)");
+            BepInExPlugin._rudderBackwardForce = this.Config.Bind("Rudder", "RudderBackwardForce", 3.0f, "Rudder backward force multiplier");
+            BepInExPlugin._rudderSpeed = this.Config.Bind("Rudder", "RudderSpeed", 2.0f, "Rudder speed multiplier");
+
             if (!BepInExPlugin._modEnabled.Value) {
                 return;
             }
@@ -127,7 +133,65 @@ namespace RideTheWind {
                 }
             }
         }
-        
+
+        [HarmonyPatch(typeof(Ship), "Start")]
+        private class ShipRudderBackwardForce {
+            private static void Prefix(Ship __instance) {
+                if (!BepInExPlugin._rudderEnabled.Value) {
+                    return;
+                }
+                
+                if (__instance == null) {
+                    if (BepInExPlugin._isDebug.Value) {
+                        MyDebug("Backward Rudder: No ship found");
+                    }
+                    return;
+                }
+                else {
+                    if (BepInExPlugin._isDebug.Value) {
+                        MyDebug("Backward Rudder: Name- " + __instance.name);
+                    }
+                }
+
+                if (BepInExPlugin._safety.Value) {
+                    if (__instance.name == "VikingShip(Clone)") {
+                        __instance.m_backwardForce = BepInExPlugin._rudderBackwardForce.Value;
+                    }
+                }
+                else {
+                    __instance.m_backwardForce = BepInExPlugin._rudderBackwardForce.Value;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Ship), "Start")]
+        private class ShipRudderSpeed {
+            private static void Prefix(Ship __instance) {
+                if (!BepInExPlugin._rudderEnabled.Value) {
+                    return;
+                }
+                if (__instance == null) {
+                    if (BepInExPlugin._isDebug.Value) {
+                        MyDebug("Rudder Speed: No ship found");
+                    }
+                    return;
+                }
+                else {
+                    if (BepInExPlugin._isDebug.Value) {
+                        MyDebug("Rudder Speed: Name- " + __instance.name);
+                    }
+                }
+                if (BepInExPlugin._safety.Value) {
+                    if (__instance.name == "VikingShip(Clone)") {
+                        __instance.m_rudderSpeed = BepInExPlugin._rudderSpeed.Value;
+                    }
+                }
+                else {
+                    __instance.m_rudderSpeed = BepInExPlugin._rudderSpeed.Value;
+                }
+            }
+        }
+
         public static void MyDebug(string str = "", bool pref = true) {
             Debug.Log((pref ? typeof(BepInExPlugin).Namespace + " " : "") + str);
         }
